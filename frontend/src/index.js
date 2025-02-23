8fb161e28d79a49ac7a4b74e2a279143800f0232
Samba BA <basamba1990@yahoo.fr>
2025-02-23 23:26:46 +0000

    Ajout de index.html dans public/ et index.js dans src/ et .....

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import UploadForm from "./UploadForm";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UploadForm />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
