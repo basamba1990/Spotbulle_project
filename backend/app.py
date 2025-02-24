987ee4a013e5f56cc9b10b832b49e6e5d0efb918
basamba1990 <basamba1990@yahoo.fr>
2025-02-24 01:16:07 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -32,4 +32,4 @@ def upload_file():
         return jsonify({"error": f"Erreur lors du traitement : {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8090, debug=True)
