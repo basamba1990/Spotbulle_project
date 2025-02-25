22996fc83fabe247337af51e244f5f1472327a35
basamba1990 <basamba1990@yahoo.fr>
2025-02-25 11:55:23 +0000

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8090";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://spotbulle-project.onrender.com";
 
 export default API_BASE_URL;
