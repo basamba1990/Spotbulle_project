c1df600f1a2e63fe2a4051e684bb4cb3592c7c41
basamba1990 <basamba1990@yahoo.fr>
2025-02-25 12:21:14 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -43,4 +43,6 @@ def upload_file():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8090)
+    # Récupérer le port à partir de la variable d'environnement ou utiliser 8090 par défaut
+    port = int(os.environ.get("PORT", 8090))
+    app.run(debug=True, host="0.0.0.0", port=port)
